b232877de5317692bc0bf189bfe65724901a2cb6
Hayden Thurman <hthurmank2c@outlook.com>
2026-05-19 16:14:10 -0500

    Removed Temporary Code

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from adafruit_servokit import ServoKit
 import time
 kit = ServoKit(channels=16)
 kit.servo[0].angle = 0
-time.sleep(1)
-for angle in range(0,180):
-	kit.servo[0].angle = angle
-	print(angle)
-	time.sleep(0.3)
+
+
+
